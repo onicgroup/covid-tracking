bca3896c69af50cc698043d8944ce12e16203fc8
Arashdeep Panesar <as2panes@uwaterloo.ca>
2020-03-26 12:29:06 -0400

    forgot to hyperlink onic group

--- components/footer/footer.component.jsx
+++ components/footer/footer.component.jsx
@@ -5,8 +5,11 @@ import { FaHeart } from 'react-icons/fa';
 
 const Footer = () => (
 	<Text>
-		Made with <FaHeart color="red"/> from the Onic Group, from a collective of 
-		social-distancing homes likely near you (but at least 6 feet apart).
+		Made with <FaHeart color="red"/> from the <a 
+			style={{color: '#add8e6'}} 
+			href="https://www.onicinc.com/"
+			target="_blank"
+		>Onic Group</a>, from a collective of social-distancing homes likely near you (but at least 6 feet apart).
 	</Text>
 );
 
