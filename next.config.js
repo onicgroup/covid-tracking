cff616ae8945875b7772a2a682fb2ba05ccf0a7c
Arashdeep Panesar <as2panes@uwaterloo.ca>
2020-03-30 22:12:38 -0400

    implemented next-pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 const withImages = require('next-images');
 const withCSS = require('@zeit/next-css');
+const withPWA = require('next-pwa')
 
-module.exports = withCSS(withImages());
+module.exports = withCSS(
+	withImages(
+		withPWA({
+			pwa: {
+				dest: 'public'
+			}
+		})
+	)
+);
--- pages/_app.js
+++ pages/_app.js
@@ -37,6 +37,17 @@ class MyApp extends App {
 							href="https://fonts.googleapis.com/css?family=Playfair+Display&display=swap"
 							rel="stylesheet"
 						/>
+						<meta charset='utf-8' />
+						<meta http-equiv='X-UA-Compatible' content='IE=edge' />
+						<meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+						<meta name='description' content='Description' />
+						<meta name='keywords' content='Keywords' />
+
+						<link rel="manifest" href="/manifest.json" />
+						<link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+						<link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+						<link rel="apple-touch-icon" href="/apple-icon.png"></link>
+						<meta name="theme-color" content="#317EFB"/>
 						<script> { process.browser && this.ensureHTTPS() } </script>
 					</Head>
 					<Component {...pageProps} />
